9cb05f202e6bcc9a5d3c6429e473d0d232be9b75
Anrádez <nradz@mint>
2014-02-24 22:54:43 +0100

    Completado canal de comunicacion de problemas.

--- channels/channels.go
+++ channels/channels.go
@@ -1,8 +1,13 @@
 package channels
 
 var clientChan = clientControlChannel{make(chan *ClientControlRequest)}
+var problemChan = problemControlChannel{make(chan *ProblemControlRequest)}
 
 func ClientControlChannel() *clientControlChannel{
 	return &clientChan
+}
+
+func ProblemControlChannel() *ProblemControlChannel{
+	return &problemChan
 }
 
