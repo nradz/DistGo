36f2baf7e5cd24f32adb624675f4d07d18f75eb6
Anrádez <nradz@mint>
2014-06-27 19:02:11 +0200

    terminado modificaciones

--- controllers/problemController/simpleProblemController.go
+++ controllers/problemController/simpleProblemController.go
@@ -8,13 +8,13 @@ import(
 
 type data interface{}
 
-type ProblemControlRequest struct{
+type problemControlRequest struct{
 	Id uint32
 	Data []string
-	Response chan ProblemControlResponse
+	Response chan problemControlResponse
 }
 
-type ProblemControlResponse struct{
+type problemControlResponse struct{
 	Alg string
 	Data data
 	Err Error
@@ -31,8 +31,7 @@ var problemState = simpleProblemState{}
 
 func SimpleProblemController(problem problems.Problem){
 
-	problemState.LastUpdate = nil
-	problemState.Clients = make(map[uint32]*clientState)
+	problemState = newSimpleProblemState()
 
 	firstUpdate := problem.Init(problemChan)
 
@@ -43,23 +42,22 @@ func SimpleProblemController(problem problems.Problem){
 
 
 	var update = problems.ProblemUpdate{}
-	var req = &ProblemControlRequest{}
-	var res = ProblemControlResponse{}
+	var req = &problemControlRequest{}
 
 	for{
 		select{
+
 		case update = <-problemChan:
 			problemState.Update(update)
 
 		case req = <-clientChan:
 			if req.Data != nil{
-				res.Err = problemState.NewResult(req.Id, req.Data)
+				problemState.NewResult(req.Id, req.Data,
+					problem problems.Problem, req.ResChan)
 			} else{
-				res.Alg, res.Data, res.Err = problemState.NewRequest(req.Id)
+				problemState.NewRequest(req.Id, req.ResChan)
 			}
-
-			req.Response <- res
-
+		
 		}
 	}
 
@@ -68,7 +66,7 @@ func SimpleProblemController(problem problems.Problem){
 
 func NewRequest(id uint32){
 
-	req := &ProblemControlRequest{id, nil, make(chan ProblemControlResponse)}
+	req := &problemControlRequest{id, nil, make(chan problemControlResponse)}
 	
 	clientChan <- req
 
@@ -79,102 +77,11 @@ func NewRequest(id uint32){
 }
 
 func NewResult(id uint32, data []string){
-	req := &ProblemControlRequest{id, data, make(chan ProblemControlResponse)}
+	req := &problemControlRequest{id, data, make(chan problemControlResponse)}
 	
 	clientChan <- req
 
 	res := <- req.Response
 
 	return res.Err
-}
-
-/////////fromClient and his functions///////////////
-
-func fromClient(req *channels.ProblemControlRequest, problem problems.Problem){
-	cState, ok := problemState.Clients[req.Id]
-	switch{
-		// New client
-		case !ok && (req.Code == 20):
-			newClient(req)
-		// Standard Request
-		case ok && (req.Code == 20):
-			newRequest(req, cState)
-		// Request with new result
-		case ok && (req.Code == 30):
-			//newResult(req, cState)
-			newResult(req, cState)
-			//Pass the data to the problem (asynchronous)
-			go problem.NewResult(req.Data)
-
-		default:
-			fmt.Println("Error in simpleProblemController.fromClient-> Code:%d OK:%t", req.Code, ok)
-	}
-
-}
-
-
-
-
-func newClient(req *channels.ProblemControlRequest){
-	cState := clientState{false, true, nil}
-
-	problemState.Clients[req.Id] = &cState
-
-	//Make the response with the algorithm and the last update
-	res := channels.ProblemControlResponse{130, problemState.Alg, problemState.LastUpdate}
-
-	req.Response <- res
-}
-
-func newRequest(req *channels.ProblemControlRequest, cState *clientState){
-
-	switch{
-
-	//Client in standby
-	case cState.Updated && !cState.Ready:
-		cState.Ready = true
-		cState.ResChan = req.Response
-
-	case !cState.Updated && !cState.Ready:
-		res := channels.ProblemControlResponse{120, "", problemState.LastUpdate}
-		req.Response <-res
-
-	default:
-		fmt.Println("Error in simpleProblemController.newRequest-> Updated: %t Ready: %t",
-			cState.Updated, cState.Ready)
-	}
-
-}
-
-
-func newResult(req *channels.ProblemControlRequest, cState *clientState){
-
-	res := channels.ProblemControlResponse{140, "", nil}
-	req.Response <- res
-}
-
-/////////fromProblem and his functions///////////////
-
-func fromProblem(update channels.ProblemUpdate){
-
-	problemState.LastUpdate = update.Data
-
-	for _, cState := range problemState.Clients{
-
-		switch{
-		
-		case cState.Ready && cState.Updated:
-			cState.Ready = false
-			cState.ResChan <- channels.ProblemControlResponse{120, "", problemState.LastUpdate}
-
-		case !cState.Ready && cState.Updated:
-			cState.Updated = false
-
-		default:
-		fmt.Println("Error in simpleProblemController.fromProblem-> Updated:",
-			cState.Updated, " Ready: ", cState.Ready)
-
-		}
-	}
-
 }
--- controllers/problemController/simpleProblemState.go
+++ controllers/problemController/simpleProblemState.go
@@ -1,7 +1,8 @@
 package problemController
 
 import(
-"github.com/nradz/DistGo/channels"
+"errors"
+"github.com/nradz/DistGo/problems"
 )
 
 type data interface{}
@@ -15,7 +16,66 @@ type simpleProblemState struct{
 type clientState struct{
 	Ready bool //A bool variable that indicate if the client is ready for a update
 	Updated bool //It indicate if the client have received the last update
-	ResChan chan channels.ProblemControlResponse /*The channel where the problemController
+	ResChan chan problemControlResponse /*The channel where the problemController
 	receive the request*/
 }
 
+func newSimpleProblemState() simpleProblemState {
+
+	sp := simpleProblemState{
+		Clients: make(map[uint32] *clientState)
+	}
+
+	return sp
+}
+
+func (sp simpleProblemState) NewRequest(id uint32, res chan problemControlResponse){
+	client, ok := simpleProblemState.Clients[id]
+	var alg string = ""
+
+	if !ok{
+		client = &clientState{false, true, nil}
+		sp.Client[id] = client
+		//the first time that the client get the algorithm
+		alg = sp.Alg
+	}
+
+	switch{
+	//Client will be set in standby
+	case client.Updated && !client.Ready:
+		client.Ready = true
+		client.ResChan = req.Response
+	//Client will be updated	
+	case !client.Updated && !client.Ready:
+		res <- problemControlResponse{alg, sp.LastUpdate, nil}
+	default:
+		err := errors.New("Unknown Error")
+		up := problemControlResponse{alg, nil, err}
+	}
+}
+
+
+func (sp simpleProblemState) NewResult(id uint32, result data, 
+  prob problems.Problem, res chan problemControlResponse){
+	//Pass the data to the problem (asynchronous)
+	go prob.NewResult(result)
+
+	res <- problemControlResponse{"", nil, nil}
+
+}
+
+func (sp simpleProblemState) Update(update problems.ProblemUpdate){
+	sp.LastUpdate = update.Data
+
+	for _, client := range sp.Clients{
+		switch{
+		//Client is in standby
+		case client.Ready && client.Updated:
+			client.Ready = false
+			client.Reschan <-problemControlResponse{"", sp.LastUpdate}
+		case !client.Ready && client.Updated:
+			client.Updated = false
+
+		}
+	}
+}
