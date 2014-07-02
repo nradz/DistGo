2033f54e3a3ab257962bf19aa7e7022edc5e93cd
Anrádez <nradz@mint>
2014-07-02 16:38:48 +0200

    test hechos

--- controllers/connectionController/connectionController.go
+++ controllers/connectionController/connectionController.go
@@ -10,17 +10,20 @@ import(
 	)
 
 type connectionController struct{
-	clientChan chan 
+	clientCon *clientController
+	probCon *problemController
 }
 
 
 var(
-	clientChan = channels.ClientControlChannel()
-	problemChan = channels.ProblemControlChannel()
 	conf = configuration.Configuration()
 	)
 
-func ConnectionController(){
+func ConnectionController(cli clientController.clientController, prob problemController.problemController){
+	return &connectionController{cli, prob}
+}
+
+func (c *connectionController) Init(){
 
 	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
 
@@ -83,139 +86,103 @@ func ConnectionController(){
 
 }
 
-func notPostMethod(sr *ServerResponse, cr ClientRequest, userAgent []string){
+func (c *connectionController) notPostMethod(sr *ServerResponse, cr ClientRequest, userAgent []string){
 	sr.Code = 0
 	
 	sr.Data = "The request method is not POST"
 }
 
-func notValidJSON(sr *ServerResponse, cr ClientRequest, userAgent []string){
+func (c *connectionController) notValidJSON(sr *ServerResponse, cr ClientRequest, userAgent []string){
 	sr.Code = 0
 	
 	sr.Data = "The JSON message is not valid"
 }
 
-func notValidCode(sr *ServerResponse, cr ClientRequest, userAgent []string){
+func (c *connectionController) notValidCode(sr *ServerResponse, cr ClientRequest, userAgent []string){
 	sr.Code = 0
 	
 	sr.Data = "Not valid Code"
 }
 
-func newClient(sr *ServerResponse, cr ClientRequest, userAgent []string){
+func (c *connectionController) newClient(sr *ServerResponse, cr ClientRequest, userAgent []string){
 	
-	control := &channels.ClientControlRequest{cr.Id, 10,
-		userAgent, make(chan channels.ClientControlResponse)}
-
-	res := clientChan.Send(control)
-
-	switch{
+	id, err := c.clientCon.NewClient(userAgent)
 	
-	case res.Code == 10:
-		sr.Code = 110
-		sr.Id = res.Id
-		sr.Data = "welcome!"
-
-
-	default:
-		fmt.Println("Error en newClient: "+string(res.Code))
-		sr.Id = res.Id
+	if err != nil{
 		sr.Code = 0
+		fmt.Println("Error en newClient:", err)
+		return
+	}
+
+	sr.Id = id
+	sr.Code = 110
+	sr.Data = "Welcome!"
+
+}
+
+func (c *connectionController) newRequest(sr *ServerResponse, cr ClientRequest, userAgent []string){	
+	sr.Id = cr.Id
+
+	err := c.clientCon.IsLogged(cr.Id, userAgent)
+	if err != nil{
+		sr.Code = 0
+		fmt.Println("Error en newRequest:", err)
+		return
+	}
+
+	alg, update, err := c.probCon.NewRequest(cr.Id)
+	if err != nil{
+		sr.Code = 0
+		fmt.Println("Error en newRequest:", err)
+		return
+	}
+
+	sr.Data = data
+
+	if alg != ""{
+		sr.Alg = alg
+		sr.code = 130
+	}else{
+		sr.Code = 120
 	}
 
 }
 
-func newRequest(sr *ServerResponse, cr ClientRequest, userAgent []string){	
+func (c *connectionController) newResult(sr *ServerResponse, cr ClientRequest, userAgent []string){
 
-	control := &channels.ClientControlRequest{cr.Id, 20,
-		userAgent, make(chan channels.ClientControlResponse)}
+	sr.Id = cr.Id
 
-	logged := clientChan.Send(control)
-
-	switch{
-
-	case logged.Code == 20:
-		problemReq := &channels.ProblemControlRequest{cr.Id, 20,
-			cr.Data, make(chan channels.ProblemControlResponse)}
-		
-		probRes := problemChan.SendRequest(problemReq)
-		
-		sr.Id = cr.Id
-		sr.Code = probRes.Code
-		sr.Alg = probRes.Alg
-		sr.Data = probRes.Data
-
-	//Error in the logging
-	case logged.Code < 10:
-		sr.Id = cr.Id
+	err := c.clientCon.IsLogged(cr.Id, userAgent)
+	if err != nil{
 		sr.Code = 0
-
-	//Internal Error
-	default:
-		fmt.Println("Error en newRequest: "+string(logged.Code))
-		sr.Id = cr.Id
-		sr.Code = 0
+		fmt.Println("Error en newRequest:", err)
+		return
 	}
+
+	err := c.probCon.NewResult(cr.Id, cr.Data)
+	if err != nil{
+		sr.Code = 0
+		fmt.println("Error en newRequest:", err)
+		return
+	}
+
+	sr.Code = 140
 
 }
 
-func newResult(sr *ServerResponse, cr ClientRequest, userAgent []string){
-
-	control := &channels.ClientControlRequest{cr.Id, 20,
-		userAgent, make(chan channels.ClientControlResponse)}
-
-	logged := clientChan.Send(control)
-
-	switch{
-	case logged.Code == 20:
-		
-		problemReq := &channels.ProblemControlRequest{cr.Id, 30,
-			cr.Data, make(chan channels.ProblemControlResponse)}
-		
-		prob := problemChan.SendRequest(problemReq)
-
-		sr.Id = cr.Id
-		sr.Code = prob.Code
-		sr.Alg = prob.Alg
-		sr.Data = prob.Data
-
-	//Error in the logging
-	case logged.Code < 10:
-		sr.Id = cr.Id
-		sr.Code = 0
-
-	//Internal Error
-	default:
-		fmt.Println("Error en newResult: "+string(logged.Code))
-		sr.Id = cr.Id
-		sr.Code = 0
-	}
-
-}
-
-func deleteClient(sr *ServerResponse, cr ClientRequest, userAgent []string){
+func (c *connectionController) deleteClient(sr *ServerResponse, cr ClientRequest, userAgent []string){
 	
-	control := &channels.ClientControlRequest{cr.Id, 30,
-		userAgent, make(chan channels.ClientControlResponse)}
+	sr.Id = cr.Id
 
-	deleted := clientChan.Send(control)
-
-	switch{
-
-	case deleted.Code == 30:
-		sr.Code = 150
-		sr.Id = cr.Id
-		sr.Data = "Goodbye!"
-
-	//Error in the logging
-	case deleted.Code < 10:
-		sr.Id = cr.Id
+	err := c.clientCon.DeleteClient(cr.Id, userAgent)
+	if err != nil{
 		sr.Code = 0
-
-	//Internal Error
-	default:
-		fmt.Println("Error en newResult: "+string(deleted.Code))
-		sr.Id = cr.Id
-		sr.Code = 0
-
+		fmt.Println("Error en newRequest:", err)
+		return
 	}
+
+	sr.Code = 150
+	sr.Data = "Goodbye!"
+
+	
 }
